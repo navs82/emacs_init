3a91a6f035e32d14b5658d8d81f0069628df9477
Naveen <NaveenWork@Naveens-Mac-mini.local>
2021-09-15 23:07:03 -0700

    init: add key-binding for super,hyper, meta keys

--- init.el
+++ init.el
@@ -17,6 +17,12 @@
 ;;                            (setq buffer-read-only t)))) ;;(setq buffer-read-only t)))
 ;;(define-key ctl-x-map "\C-q" 'view-mode)
 (define-key ctl-x-map "\C-q" 'read-only-mode)
+
+;; set keys for Apple keyboard, for emacs in OS X
+(setq mac-command-modifier 'meta) ; make cmd key do Meta
+(setq mac-option-modifier 'super) ; make opt key do Super
+(setq mac-control-modifier 'control) ; make Control key do Control
+(setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Configuration/Customization:
 ;; Defines global variables that are later used to customize and set
